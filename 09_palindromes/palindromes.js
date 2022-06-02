d4e03a9c78953c480ac80dae66fcd63872a464ed
Nutty-Nuts <vegagerard1130@gmail.com>
2022-06-03 00:03:53 +0800

    palindromes exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let lowercaseText = text.toLowerCase();
+    let initArr = Array.from(lowercaseText);
 
+    let normalArr = [];
+    let reverseArr = [];
+    let sampleArr = [];
+
+    for (e of initArr) {
+        if (e != " " && e != "!" && e != "." && e != ",") {
+            normalArr.push(e);
+            sampleArr.push(e);
+        }
+    }
+
+    for (e of normalArr) {
+        if (e != " " && e != "!" && e != "." && e != ",") {
+            reverseArr.push(sampleArr.pop());
+        }
+    }
+
+    let normal = normalArr.toString();
+    let reverse = reverseArr.toString();
+
+    console.log(normal);
+    console.log(reverse);
+
+    if (normal === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
