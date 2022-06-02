ef719785b26c31b79629f7e211c5809145425544
Nutty-Nuts <vegagerard1130@gmail.com>
2022-06-03 00:26:38 +0800

    get the titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const getTheTitles = function (book) {
+    let bookArray = Array.from(book);
+    let titleArray = [];
 
+    for (e of book) {
+        let element = Object.values(e);
+
+        titleArray.push(element[0]);
+    }
+
+    return titleArray;
 };
+const books = [
+    {
+        title: "Book",
+        author: "Name",
+    },
+    {
+        title: "Book2",
+        author: "Name2",
+    },
+];
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
