c062192dbc44d839bd7422201748f71154367a0a
Nutty-Nuts <vegagerard1130@gmail.com>
2022-06-03 00:27:00 +0800

    removed some code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -10,18 +10,6 @@ const getTheTitles = function (book) {
 
     return titleArray;
 };
-const books = [
-    {
-        title: "Book",
-        author: "Name",
-    },
-    {
-        title: "Book2",
-        author: "Name2",
-    },
-];
-
-getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
