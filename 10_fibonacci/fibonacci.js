d86ce66976ab7e0c2f33128d5daa2fa29465d201
Nutty-Nuts <vegagerard1130@gmail.com>
2022-06-03 00:14:52 +0800

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let fibonacciSeq = [1, 1];
 
+    num = parseInt(num);
+
+    if (num <= 2 && num > 0) {
+        return 1;
+    } else if (num > 0) {
+        for (let i = 1; i <= num - 2; i++) {
+            fibonacciSeq.push(fibonacciSeq[i] + fibonacciSeq[i - 1]);
+        }
+
+        return fibonacciSeq.pop();
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
