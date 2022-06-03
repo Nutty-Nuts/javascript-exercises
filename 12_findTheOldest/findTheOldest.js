701b317136fd30f947cd4cb71b42ce44241560f3
Nutty-Nuts <vegagerard1130@gmail.com>
2022-06-03 22:35:07 +0800

    find the oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let peopleArr = Array.from(people);
 
+    const oldestPeople = peopleArr.sort(function (a, b) {
+        let currentYear = new Date().getFullYear();
+        let ageA = 0;
+        let ageB = 0;
+
+        if (typeof a.yearOfDeath === "undefined") {
+            ageA = currentYear - a.yearOfBirth;
+            console.log("undefined");
+        } else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+
+        if (typeof b.yearOfDeath === "undefined") {
+            ageB = currentYear - b.yearOfBirth;
+        } else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        console.log(`${a.name}: ${ageA} ${b.name}: ${ageB}`);
+
+        if (ageA > ageB) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    return oldestPeople.pop();
 };
 
 // Do not edit below this line
